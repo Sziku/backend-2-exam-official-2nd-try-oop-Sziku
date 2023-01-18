88644fbdc3303525c1940f2d95431468ea97442e
Sziku <105989158+Sziku@users.noreply.github.com>
2023-01-18 12:13:42 +0100

    create animal class

--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -1,4 +1,15 @@
 package com.codecool.farm.animal;
 
-public class Animal {
+public abstract class Animal {
+    private final int size;
+
+    public Animal() {
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public abstract void feed();
 }
